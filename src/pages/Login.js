5acb891ca5625b0565d1e56b54088c6cd3faf60c
Yuliia Skabitskaya <yulik777@i.ua>
2022-09-08 16:33:24 +0300

    added states

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,15 @@
-import { Typography } from "@mui/material";
+import { Typography,TextField, Button } from "@mui/material";
 import logo from "../assets/logo.svg"
+import {useState} from 'react';
 
 
 const Login = () => {
+
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+
+console.log('fields', email,password);
+
     return (
     <main className='wrapper dark'>
     <form className="login-form">
@@ -12,6 +19,28 @@ const Login = () => {
     <Typography className="sub-title" variant="subtitle1" component="h2" color=" ">
         Dashbord Kit
     </Typography>
+    <Typography className="sub-title-2" variant="body" component="p">
+          Enter your email and password below
+        </Typography>
+     
+        <TextField 
+          margin="dense"
+          fullWidth
+          label="Email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField 
+          margin="dense"
+          fullWidth
+          label="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+    <Button variant="contained" color="primary" fullWidth
+    sx={{marginTop: 2,}}>
+        Log In
+    </Button>
      </form>
     </main>
     );
